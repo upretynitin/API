b32e1566de7a904865b0c3c5ce8aa4511ecce12d
upretynitin <uprety1999@gmail.com>
2023-12-15 13:55:14 +0530

    uu

--- routes/web.js
+++ routes/web.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const UserController = require('../controllers/UserController')
+const CategoryController = require('../controllers/CategoryController')
+const ProductController = require('../controllers/ProductController')
 const router = express.Router()
 
 //usercontroller
@@ -8,7 +10,19 @@ router.post('/userinsert',UserController.userinsert)
 router.get('/getuserdetails',UserController.getuserdetails)
 router.post('/verifylogin',UserController.verifylogin)
 
+//categorycontroller
+router.post('/categoryinsert',CategoryController.categoryinsert)
+router.get('/categorydisplay',CategoryController.categorydisplay)
+router.get('/categoryview',CategoryController.categoryview)
+router.post('/categoryupdate',CategoryController.categoryupdate)
+router.get('/categorydelete',CategoryController.categorydelete)
 
+//product controller
+router.get('/getallproduct',ProductController.getallproduct)
+router.post('/createproduct',ProductController.createproduct)
+router.get('/getallproductdetail',ProductController.getallproductdetail)
+router.post('/updateproduct',ProductController.updateproduct)
+router.get('/deleteproduct',ProductController.deleteproduct)
 
 
 
